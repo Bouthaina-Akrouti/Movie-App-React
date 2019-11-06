70804cc53d5dd14c8db2dd46082872280aca5776
bothazerty12 <bouthaynaakrouti522@gmail.com>
2019-11-06 12:31:36 +0100

    Updates

---
+++ precache-manifest.504b74ded57b5ca29b167c25264eaec1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faa72470ec27bb2328b743871e5c941b",
+    "url": "/Movie-App-React/index.html"
+  },
+  {
+    "revision": "a1c338c532fa20d146e9",
+    "url": "/Movie-App-React/static/css/main.389347a7.chunk.css"
+  },
+  {
+    "revision": "75605abf6c65c76abe7c",
+    "url": "/Movie-App-React/static/js/2.92fea874.chunk.js"
+  },
+  {
+    "revision": "a1c338c532fa20d146e9",
+    "url": "/Movie-App-React/static/js/main.a7b12a9f.chunk.js"
+  },
+  {
+    "revision": "97ef5e4ad1a597e3e649",
+    "url": "/Movie-App-React/static/js/runtime-main.0240acd8.js"
+  }
+]);
